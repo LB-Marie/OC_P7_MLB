e694c0e6f845132fbf0c5de8648d31713b71a8ad
Marie Le Brasseur <marie.lebrasseur@icloud.com>
2022-02-06 09:28:11 +0100

    Creation of the files used for the API deployment

---
+++ API_deployment.py
@@ -0,0 +1,60 @@
+###############################################################################
+##                              Library imports                              ##
+###############################################################################
+import uvicorn
+from fastapi import FastAPI
+import pickle
+from pydantic import BaseModel
+import numpy as np
+#
+###############################################################################
+##                           Creation of the app object                      ##
+###############################################################################
+#
+app = FastAPI()
+pickle_in = open("Scoring_model.pkl","rb")
+classifier=pickle.load(pickle_in)
+#
+###############################################################################
+##         Creation of class to define the parameters for the API            ##
+###############################################################################
+#
+class Scoring_data(BaseModel):
+    EXT_SOURCE_2 : float
+    EXT_SOURCE_3 : float
+    EXT_SOURCE_1 : float
+    DAYS_BIRTH : float
+    DAYS_EMPLOYED_PERC : float
+    DAYS_ID_PUBLISH : float
+    DAYS_REGISTRATION : float
+    BURO_DAYS_CREDIT_MEAN : float
+    BURO_DAYS_CREDIT_MAX : float
+    INSTAL_DAYS_ENTRY_PAYMENT_MEAN : float
+    ACTIVE_DAYS_CREDIT_MAX : float
+    DAYS_LAST_PHONE_CHANGE : float
+    INSTAL_DAYS_ENTRY_PAYMENT_SUM : float
+    BURO_DAYS_CREDIT_ENDDATE_MEAN : float
+    PREV_APP_CREDIT_PERC_MEAN : float
+    REGION_POPULATION_RELATIVE : float
+    APPROVED_APP_CREDIT_PERC_MAX : float
+    APPROVED_AMT_ANNUITY_MAX : float
+    PREV_HOUR_APPR_PROCESS_START_MEAN : float
+    AMT_GOODS_PRICE : float
+    ACTIVE_DAYS_CREDIT_MIN : float
+    CLOSED_DAYS_CREDIT_MIN : float
+    ACTIVE_DAYS_CREDIT_ENDDATE_MAX : float
+    POS_COUNT : float
+#
+###############################################################################
+##    Creation of post for the API go get the probability and predictions    ##
+###############################################################################
+#       
+@app.post('/predict')
+def predict_score(data:Scoring_data):
+    data = data.dict()
+    values = np.array(list(data.values())).reshape(1, -1)
+    prediction = int(classifier.predict(values))
+    probability = round(classifier.predict_proba(values)[0,prediction], 2)
+    return {
+        'prediction': str(prediction), 
+        'probability' : str(probability) }
